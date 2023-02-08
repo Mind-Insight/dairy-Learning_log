254f41950e599a0740a4c3ec027cc9df66e07554
Timofey <nsk.timka2077@gmail.com>
2023-02-08 12:55:41 +0200

    added button 'show details' on news page

--- learning_log/news/urls.py
+++ learning_log/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name="news_detail")
 ]
--- learning_log/news/views.py
+++ learning_log/news/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 
 def news_home(request):
     news = Articles.objects.order_by('title')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 
 def create(request):
