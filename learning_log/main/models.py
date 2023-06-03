8e6f7f503c3aa456604008b677dcbb8bce39d461
Timofey <nsk.timka2077@gmail.com>
2023-06-03 20:09:00 +0200

    new venv for linux

--- learning_log/main/models.py
+++ learning_log/main/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class Topic(models.Model):
     """Тема, которую изучает пользователь"""
+
     text = models.CharField(max_length=200)  # CharField - для хранения текста
-    date_added = models.DateTimeField(auto_now_add=True)  # DateTimeField - для хранения даты и времени
+    date_added = models.DateTimeField(
+        auto_now_add=True
+    )  # DateTimeField - для хранения даты и времени
     # auto_add_now=True - присваивание атрибуту текущую дату и время при создании новой темы
 
     def __str__(self):
@@ -13,6 +16,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Информация, изученная пользователем по теме"""
+
     # ForeighKey - содержит ссылку на другую запись в базе данных
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
@@ -20,7 +24,7 @@ class Entry(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'entries'
+        verbose_name_plural = "entries"
 
     def __str__(self):
         return f"{self.text[:50]}"
